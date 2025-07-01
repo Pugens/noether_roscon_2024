5bf0cca1fabb6e6f8e37e13e945143ca7cb63806
Eugenio Bernardi <eugenio.bernardi@proton.me>
2025-07-01 11:32:44 +0000

    deleted example files

--- include/noether_roscon_2024/exercise_2c/no_op_tool_path_planner.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <noether_tpp/core/tool_path_planner.h>
-
-namespace noether
-{
-// TODO: create a class called NoOpToolPathPlanner that inherits the ToolPathPlanner base class
-
-}  // namespace noether
